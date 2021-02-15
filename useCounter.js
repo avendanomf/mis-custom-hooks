c4eff7abdd57ec7e17dbe1c75313586d2c3ed45e
ecm6802i <miguel.avendano@claro.co.co>
2021-02-15 11:25:08 -0500

    primer commit

---
+++ useCounter.js
@@ -0,0 +1,31 @@
+import {useState } from "react";
+
+
+export const useCounter = ( initialState = 10) => {
+
+   const  [counter,setCounter] = useState(initialState);
+
+   const increment = (factor = 1) =>{
+       setCounter (counter + factor);
+   }
+
+   const decrement = (factor = 1) =>{
+       setCounter (counter - factor);
+   }
+
+   const decrementPositiveNum = (factor = 1) =>{
+       setCounter (counter > 1 ? counter - factor: counter);
+   }
+
+   const reset = () =>{
+       setCounter (initialState);
+   }
+
+   return {
+       counter,
+       increment,
+       decrement,
+       decrementPositiveNum,
+       reset
+   }
+}
